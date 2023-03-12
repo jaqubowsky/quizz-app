687939b4cf309862f3b686c1215152d5bfb2b959
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-12 16:50:42 +0100

    Add fetching API and creating question object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import Home from "./Pages/Home";
+import Quiz from "./Pages/Quiz";
 import GlobalStyle from "./Styles/Global";
 import Theme from "./Styles/Theme";
+import { nanoid } from "nanoid";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [questionsData, setQuestionsData] = useState([]);
+
+useEffect(() => {
+  fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    .then((res) => res.json())
+    .then((data) => {
+      const formattedData = data.results.map((question) => ({
+        question: question.question,
+        correct_answer: question.correct_answer,
+        all_answers: [...question.incorrect_answers, question.correct_answer],
+        id: nanoid(),
+      }));
+      setQuestionsData(formattedData);
+    });
+}, []);
+
   return (
     <Theme>
       <GlobalStyle />
-      <Home />
+      {!isClicked ? (
+        <Home handleClick={() => setIsClicked(true)} />
+      ) : (
+        <Quiz questionsData={questionsData} />
+      )}
     </Theme>
   );
 }
