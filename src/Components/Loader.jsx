e3ad707c62d99fb10bfdb3b81f58c5b51e7b082e
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-15 15:31:58 +0100

    Add loader component

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,29 @@
+import { Oval } from "react-loader-spinner";
+import styled from "styled-components";
+
+const StyledLoader = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Load = () => {
+  return (
+    <StyledLoader>
+      <Oval
+        height={80}
+        width={80}
+        color= "blue"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="white"
+        strokeWidth={5}
+        strokeWidthSecondary={1}
+      />
+    </StyledLoader>
+  );
+};
